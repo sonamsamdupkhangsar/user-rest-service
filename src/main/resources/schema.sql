9ae3d9d36d809e5dede30ccc684158ee9ea7c137
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-06-16 13:36:35 -0600

    remove drop table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,2 @@
-drop table if exists "User";
 CREATE TABLE if not exists My_User (id UUID PRIMARY KEY, first_name varchar, last_name varchar, email varchar,
  birth_date timestamp, profile_photo varchar, gender_id UUID);
