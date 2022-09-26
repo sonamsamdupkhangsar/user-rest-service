3a1d1edfa919659ad32a747079bafa8e0729dcdd
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-09-26 10:54:35 -0600

    set eureka client

--- src/main/java/me/sonam/user/Application.java
+++ src/main/java/me/sonam/user/Application.java
@@ -5,11 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
