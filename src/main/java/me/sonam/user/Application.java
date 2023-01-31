4822a30cc8945db5352079474b8cfe87d6580e6f
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-01-30 17:01:09 -0700

    add readme

--- src/main/java/me/sonam/user/Application.java
+++ src/main/java/me/sonam/user/Application.java
@@ -3,7 +3,6 @@ package me.sonam.user;
 import io.r2dbc.spi.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,27 +10,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-
-import javax.annotation.PostConstruct;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "me.sonam")
 public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
-    @Value("${EUREKA_USER}")
-    private String user;
-
-    @Value("${EUREKA_PASSWORD}")
-    private String password;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @PostConstruct
-    public void logProperty() {
-        LOG.info("eureka user: {}, password: {}", user, password);
     }
 
     @Bean()
@@ -46,5 +35,22 @@ public class Application {
         return initializer;
     }
 
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        LOG.info("allow cors filter");
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedHeader("Content-Type");
+        corsConfig.addAllowedHeader("api_key");
+        corsConfig.addAllowedHeader("Authorization");
 
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 }
