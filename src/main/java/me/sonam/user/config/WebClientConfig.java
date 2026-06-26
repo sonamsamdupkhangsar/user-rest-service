d79a039803a19a9a7d11c23e0186c0251cdd283c
sonamsamduphangsar <sonamhava@gmail.com>
2026-06-26 15:57:06 -0600

    remove profile from config

--- src/main/java/me/sonam/user/config/WebClientConfig.java
+++ src/main/java/me/sonam/user/config/WebClientConfig.java
@@ -12,10 +12,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Profile({"eureka", "non-eureka"})
 @Configuration
 public class WebClientConfig {
     @Value("${account-rest-service.context}")
